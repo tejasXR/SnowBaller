c1bb64b1a5eb575a64ee3f4a0a7942a3656760ea
tejasXR <tejasshroff24@gmail.com>
2025-05-20 08:27:34 -0400

    Adding a NetworkPlayerManager class to throw events in  a more accurate manner after a player has been initialized

--- Assets/_App/Scripts/Network/NetworkPlayer.cs
+++ Assets/_App/Scripts/Network/NetworkPlayer.cs
@@ -1,16 +1,34 @@
+using System;
 using Fusion;
 using UnityEngine;
 
 namespace Snowballers.Network
 {
-    public class NetworkPlayer : NetworkBehaviour
+    public class NetworkPlayer : NetworkBehaviour, IPlayerJoined, IPlayerLeft
     {
         [SerializeField] private NetworkObject networkObject;
-    
-        public override void Spawned()
+
+        private PlayerRef _playerRef;
+        private NetworkPlayerManager _networkPlayerManager;
+        
+        public void PlayerJoined(PlayerRef player)
         {
-            base.Spawned();
-            Runner.SetPlayerObject(Runner.LocalPlayer, networkObject);
+            _playerRef = player;
+            Runner.SetPlayerObject(player, networkObject);
+
+            _networkPlayerManager = FindAnyObjectByType<NetworkPlayerManager>(FindObjectsInactive.Include);
+            if (!_networkPlayerManager)
+            {
+                Debug.LogError("Can't find Network Player Manager in scene!");
+                return;
+            }
+            
+            _networkPlayerManager.RegisterPlayer(player);
+        }
+
+        public void PlayerLeft(PlayerRef player)
+        {
+            _networkPlayerManager.UnregisterPlayer(_playerRef);
         }
     }
 }
---
+++ Assets/_App/Scripts/Network/NetworkPlayerManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Fusion;
+
+namespace Snowballers.Network
+{
+    public class NetworkPlayerManager : NetworkBehaviour
+    {
+        public event Action<PlayerRef, NetworkPlayer> PlayerJoinedCallback;
+        public event Action<PlayerRef, NetworkPlayer> PlayerLeftCallback;
+
+        // Intentionally non-networked
+        private readonly List<PlayerRef> _playerRefs = new List<PlayerRef>();
+        
+        public void RegisterPlayer(PlayerRef playerRef)
+        {
+            if (_playerRefs.Contains(playerRef))
+            {
+                return;
+            }
+            
+            var playerRig = NetworkUtils.GetPlayerRigFromRef(Runner, playerRef);
+            _playerRefs.Add(playerRef);
+            PlayerJoinedCallback?.Invoke(playerRef, playerRig);
+        }
+
+        public void UnregisterPlayer(PlayerRef playerRef)
+        {
+            if (!_playerRefs.Contains(playerRef))
+            {
+                return;
+            }
+            
+            var playerRig = NetworkUtils.GetPlayerRigFromRef(Runner, playerRef);
+            _playerRefs.Remove(playerRef);
+            PlayerLeftCallback?.Invoke(playerRef, playerRig);
+        }
+    }
+}
