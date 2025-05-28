d9f6e7d8f51fe76efedc2ef3d0be672c36f11cdc
tejasXR <tejasshroff24@gmail.com>
2025-05-28 13:32:13 -0400

    Updating how teleport works

--- Assets/_App/Scripts/Player/Player.cs
+++ Assets/_App/Scripts/Player/Player.cs
@@ -20,6 +20,9 @@ namespace Snowballers
             
             rb.isKinematic = true;
             
+            playerLocomotion.transform.localPosition = Vector3.zero;
+            playerLocomotion.transform.localRotation = Quaternion.Euler(Vector3.zero);
+            
             hardwareRig.Teleport(position);
 
             await UniTask.WaitForEndOfFrame();
